a72583dbf808d10a413598d529b7630a6c5f139c
Somaycon <contato.somaycon@gmail.com>
2026-09-16 10:20:28 -0300

    feat: Geração de código para url

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"crypto/rand"
+	"math/big"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+type Url struct {
+	Code string `json:"code"`
+	Url  string `json:"url"`
+}
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func main() {
 	r := gin.Default()
@@ -17,8 +26,30 @@ func main() {
 
 	r.POST("/url", func(ctx *gin.Context) {
 		url := ctx.DefaultQuery("url", "url")
-		ctx.String(http.StatusOK, url)
+		code, err := GenerateCode(6)
+		if err != nil {
+			ctx.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		newUrl := Url{
+			Code: code,
+			Url:  url,
+		}
+		ctx.JSON(http.StatusOK, newUrl)
 	})
 
 	r.Run()
+}
+
+func GenerateCode(length int) (string, error) {
+	b := make([]byte, length)
+	for i := range b {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			return "", err
+		}
+		b[i] = charset[num.Int64()]
+	}
+	return string(b), nil
 }
